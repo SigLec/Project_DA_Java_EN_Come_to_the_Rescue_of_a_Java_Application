763bd72b1d88949b8f19fc4ecea3d68f6ff2ada0
sleca <sleca@LAPTOP-UB966L17.home>
2021-03-26 12:15:46 +0100

    Appel de la méthode getFileIn et lecture des résultats dans l'ArrayList

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,8 +1,16 @@
 package com.hemebiotech.analytics;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String args[]) {
 
+		FileManagement file = new FileManagement("C:\\Users\\sleca\\symptoms.txt");
+
+		ArrayList<String> result = new ArrayList<String>();
+		result = (ArrayList<String>) file.getFileIn();
+
+		System.out.println(result); // Lecture des données de file dans l'ArrayList result
 	}
 }
