55f3ffe5aaca7d257eacbe5ce625736dbcfbc4dd
sleca <sleca@LAPTOP-UB966L17.home>
2021-03-30 10:40:00 +0200

    Modificiation chemin du fichier dans Main

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String args[]) {
 
-		FileManagement file = new FileManagement("C:\\Users\\sleca\\symptoms.txt");
+		FileManagement file = new FileManagement("symptoms.txt");
 
 		ArrayList<String> result = new ArrayList<String>();
 		result = (ArrayList<String>) file.getFileIn();
